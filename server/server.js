35726cb997a12ad6fd460e53c5f7fc532e4b9a44
Maghish <mail4mithran@gmail.com>
2024-03-10 13:45:46 +0530

    Added Backend pt-2

--- server/server.js
+++ server/server.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Connected to MongoDB Successfully");
+  app.listen(process.env.PORT, () => {
+    console.log("Listening on port " + process.env.PORT);
+  });
+});
