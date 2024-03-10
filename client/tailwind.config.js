0100c567991cccc85233c9696027a9a36fe5db02
Maghish <mail4mithran@gmail.com>
2024-03-10 13:48:12 +0530

    Modified tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        palette1: "#F5F0BB",
+        palette2: "#C4DFAA",
+        palette3: "#90C8AC",
+        palette4: "#73A9AD",
+      },
+    },
   },
   plugins: [],
 };
