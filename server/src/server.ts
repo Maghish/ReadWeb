3d37baa6a101f0820e7bb9aab160af01a449f578
Maghish <mail4mithran@gmail.com>
2024-03-11 14:13:18 +0530

    Changing to TS pt-2

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 
-// import homeRoute from "./routes/home";
+import homeRoute from "./routes/home";
 
 dotenv.config();
 const app = express();
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.use("/api", homeRoute);
+app.use("/api", homeRoute);
 
 mongoose.connect(process.env.MONGO_URI!).then(() => {
   console.log("Connected to MongoDB Successfully");
