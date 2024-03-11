1adce6ed66e5edfdb87359242356aa3a61f162b2
Maghish <mail4mithran@gmail.com>
2024-03-11 22:00:41 +0530

    Fixed minor bug

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api", homeRoute);
+app.use("/api/auth", homeRoute);
 
 mongoose.connect(process.env.MONGO_URI!).then(() => {
   console.log("Connected to MongoDB Successfully");
