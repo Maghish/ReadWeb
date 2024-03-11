3996a3ab72e7fe68a02c782804c23c1269b426d8
Maghish <mail4mithran@gmail.com>
2024-03-11 12:40:13 +0530

    Did something

--- server/server.js
+++ server/server.js
@@ -4,10 +4,14 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 dotenv.config();
 
+const homeRoute = require("./routes/home");
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api", homeRoute);
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("Connected to MongoDB Successfully");
