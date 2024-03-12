42c2cf291837891bc0ff460b7f231392fb577f6c
Maghish <mail4mithran@gmail.com>
2024-03-12 09:20:29 +0530

    Formatted bookManage.ts

--- server/src/controllers/bookManage.ts
+++ server/src/controllers/bookManage.ts
@@ -1,5 +1,14 @@
+import { Request, Response } from "express";
+
 // Models
 
 import bookModel from "../models/bookModel";
+
+function getBook(req: Request, res: Response) {}
+function createBook(req: Response, res: Response) {}
+function editBook(req: Response, res: Response) {}
+function deleteBook(req: Response, res: Response) {}
+function rateBook(req: Response, res: Response) {}
+function writeReviewOnBook(req: Response, res: Response) {}
 
 export default {};
