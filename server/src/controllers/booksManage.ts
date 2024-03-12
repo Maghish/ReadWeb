dd7362de619b7659504fbc49d2c158bb808faefd
Maghish <mail4mithran@gmail.com>
2024-03-12 19:27:39 +0530

    Change functions parameter type

--- server/src/controllers/booksManage.ts
+++ server/src/controllers/booksManage.ts
@@ -15,12 +15,12 @@ async function getAllBooks(req: Request, res: Response) {
   }
 }
 async function getBook(req: Request, res: Response) {}
-async function createBook(req: Response, res: Response) {}
-async function editBook(req: Response, res: Response) {}
-async function deleteBook(req: Response, res: Response) {}
-async function rateBook(req: Response, res: Response) {}
-async function writeReviewOnBook(req: Response, res: Response) {}
-async function addTagsToBook(req: Response, res: Response) {}
-async function editTagsInBook(req: Response, res: Response) {}
+async function createBook(req: Request, res: Response) {}
+async function editBook(req: Request, res: Response) {}
+async function deleteBook(req: Request, res: Response) {}
+async function rateBook(req: Request, res: Response) {}
+async function writeReviewOnBook(req: Request, res: Response) {}
+async function addTagsToBook(req: Request, res: Response) {}
+async function editTagsInBook(req: Request, res: Response) {}
 
 export default {};
