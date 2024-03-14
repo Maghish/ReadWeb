83c949705e597af5c0e8aab8cfa1294dc1e0547f
Maghish <mail4mithran@gmail.com>
2024-03-14 21:08:05 +0530

    Redesigned logout button with a profile icon

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,3 +1,6 @@
+import { FaRegCircleUser } from "react-icons/fa6";
+import { IconContext } from "react-icons";
+
 function Navbar() {
   return (
     <div className="flex flex-row items-center w-full h-[60px] bg-palette9 px-[50px] py-[17px]">
@@ -18,10 +21,15 @@ function Navbar() {
           <a href="https://github.com/Maghish/ReadWeb.git">Github</a>
         </li>
       </ul>
-      <button className=""></button>
-      <button className="ml-auto bg-inherit border-2 border-palette1 w-[110px] h-[45px] rounded-lg font-Ubuntu text-palette1">
+      <div className="ml-auto group flex flex-col items-end cursor-pointer w-auto">
+        <IconContext.Provider value={{ size: "25px", color: "#FFFFFF" }}>
+          <FaRegCircleUser />
+        </IconContext.Provider>
+        <span className="absolute w-max top-[50px] right-8 scale-0 border-2 border-[#4b4b4b] bg-[#373737] p-2 text-xs text-palette2 group-hover:scale-100"></span>
+      </div>
+      {/* <button className="bg-inherit border-2 border-palette1 w-[110px] h-[45px] rounded-lg font-Ubuntu text-palette1">
         Logout
-      </button>
+      </button> */}
     </div>
   );
 }
