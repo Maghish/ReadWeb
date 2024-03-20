cf2668c2b5d2b9b7b835f0dce79b5b4fee287cb8
Maghish <mail4mithran@gmail.com>
2024-03-20 11:58:46 +0530

    Created Explore page

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -14,7 +14,9 @@ function UserProfileButton(props: UserProfileButtonComponentProps) {
       <IconContext.Provider value={{ size: "25px", color: "#FFFFFF" }}>
         <FaRegCircleUser />
       </IconContext.Provider>
-      <span className="absolute w-max top-[50px] right-8 scale-0 border-2 border-[#4b4b4b] bg-[#373737] p-2 text-xs text-palette2 group-hover:scale-100">{props.userCred.username}</span>
+      <span className="absolute w-max top-[50px] right-8 scale-0 border-2 border-[#4b4b4b] bg-[#373737] p-2 text-xs text-palette2 group-hover:scale-100">
+        {props.userCred.username}
+      </span>
     </div>
   );
 }
@@ -66,7 +68,7 @@ function LoginSignupButtons() {
 
 function Navbar(props: NavbarComponentProps) {
   return (
-    <div className="flex flex-row items-center w-full h-[60px] bg-palette9 px-[50px] py-[17px]">
+    <div className="fixed flex flex-row items-center w-full h-[60px] bg-palette9 px-[50px] py-[17px]">
       <a
         className="font-Ubuntu font-bold text-xl text-palette1 cursor-pointer"
         href="/"
@@ -74,9 +76,13 @@ function Navbar(props: NavbarComponentProps) {
         ReadWeb
       </a>
       <ul className="flex flex-row ml-[70px] items-center">
-        <li className="font-Ubuntu text-palette2 mr-6 cursor-pointer transition duration-150 ease-in hover:text-palette5">
-          <a href="/about">About</a>
-        </li>
+        {props.page === "Guest" ? (
+          ""
+        ) : (
+          <li className="font-Ubuntu text-palette2 mr-6 cursor-pointer transition duration-150 ease-in hover:text-palette5">
+            <a href="/mybooks">My Books</a>
+          </li>
+        )}
         <li className="font-Ubuntu text-palette2 mr-6 cursor-pointer transition duration-150 ease-in hover:text-palette5">
           <a href="/explore">Explore</a>
         </li>
