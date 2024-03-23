a737f7cd10048aabf1e92d91d12d0ed6ab24dae5
Maghish <mail4mithran@gmail.com>
2024-03-23 13:52:06 +0530

    Created auth model

---
+++ server/src/models/auth.model.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    // General Information
+    username: { type: String, required: true }, 
+    email: { type: String, required: true }, 
+    password: { type: String, required: true }, 
+    
+    // Application based information
+    favoriteBooks: { type: [], required: true }, 
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("UserModel", userSchema);
