65f3f21c68459347caba5f4c24c4d83c3b94ec3e
Maghish <mail4mithran@gmail.com>
2024-03-23 13:51:34 +0530

    Edited server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,25 @@
-console.log("Hello world!");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import authRoute from "./routes/auth.route";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use("/api/auth", authRoute);
+
+mongoose.connect(process.env.MONGO_URI!).then(() => {
+  console.log("Connected to MongoDB Successfully");
+  app.listen(process.env.PORT, () => {
+    console.log("Listening on port " + process.env.PORT);
+  });
+});
