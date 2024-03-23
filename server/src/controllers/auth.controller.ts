85ed14e50fa6fdedb65de3752a822912446601b9
Maghish <mail4mithran@gmail.com>
2024-03-23 14:05:18 +0530

    Created login function

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import UserModel from "../models/auth.model";
 import { sign } from "jsonwebtoken";
-import bcrypt from "bcryptjs";
+import { genSalt, hash, compare } from "bcryptjs";
 
 function generateToken(id: any) {
   return sign({ id }, process.env.JWT_SECRET!, {
@@ -15,36 +15,39 @@ async function signupUser(req: Request, res: Response) {
 
     UserModel.findOne({
       username: username,
-    }).then(response => {
-      if (response) {
-        return res.status(400).json({
-          message:
-            "User already exist with same username, try different username",
-        });
-      }
-    }).catch((error: any) => {});
+    })
+      .then((response) => {
+        if (response) {
+          return res.status(400).json({
+            message:
+              "User already exist with same username, try different username",
+          });
+        }
+      })
+      .catch((error: any) => {});
 
     UserModel.findOne({
       email: email,
-    }).then((response) => {
-      if (response) {
-        return res.status(400).json({
-          message:
-            "User already exist with same email, try different email",
-        });
-      }
-    }).catch((error: any) => {});
+    })
+      .then((response) => {
+        if (response) {
+          return res.status(400).json({
+            message: "User already exist with same email, try different email",
+          });
+        }
+      })
+      .catch((error: any) => {});
 
     // Hash Password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const salt = await genSalt(10);
+    const hashedPassword = await hash(password, salt);
 
     const newUser = new UserModel({
       username: username,
-      email: email, 
+      email: email,
       password: hashedPassword,
-      favoriteBooks: []
-    })
+      favoriteBooks: [],
+    });
 
     await newUser.save();
 
@@ -53,10 +56,28 @@ async function signupUser(req: Request, res: Response) {
       token: generateToken(newUser._id),
       userData: newUser,
     });
-
   } catch (error: any) {
     return res.status(400).json({ message: error });
   }
 }
 
-export { signupUser };
+async function loginUser(req: Request, res: Response) {
+  try {
+    const { email, password } = req.body;
+    const foundUser = await UserModel.findOne({ email: email });
+
+    if (foundUser && (await compare(password, foundUser!.password))) {
+      res.status(200).json({
+        message: "Successfully authenticated user",
+        token: generateToken(foundUser._id),
+        userData: foundUser,
+      });
+    } else {
+      res.status(200).json({ message: "Password not correct" });
+    }
+  } catch (error: any) {
+    return res.status(400).json({ message: error });
+  }
+}
+
+export { signupUser, loginUser };
