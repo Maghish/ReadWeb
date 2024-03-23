8ab786a4534fdef9549cebf8a6feb0dcf7d98bd5
Maghish <mail4mithran@gmail.com>
2024-03-23 20:35:13 +0530

    Created route for getUser function

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { signupUser, loginUser } from "../controllers/auth.controller";
+import { signupUser, loginUser, getUser } from "../controllers/auth.controller";
+
+import protect from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.post('/signup', signupUser);
 router.post('/login', loginUser);
+router.get('/getuser', protect, getUser);
+// GET getcurrentuser
+// POST edituser
+// DELETE signout
 
 export default router
