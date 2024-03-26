d831340045458e2972b09d775d6f74a8deb46b54
Maghish <mail4mithran@gmail.com>
2024-03-26 11:28:46 +0530

    General Mockup

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,9 +8,56 @@ function Home() {
   return (
     <>
       <Navbar />
-      <p>{userCred.username}</p>
+      <div className="mt-14 px-16 pb-16 flex flex-col items-center gap-y-16">
+        <div className="w-full flex flex-col">
+          <span className="text-palette9 font-Roboto font-semibold text-2xl mb-7">
+            New Releases
+          </span>
+          <div className="flex flex-row gap-x-6 overflow-x-auto min-w-full pb-4">
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+          </div>
+        </div>
+        <div className="w-full flex flex-col">
+          <span className="text-palette9 font-Roboto font-semibold text-2xl mb-7">
+            Continue Reading
+          </span>
+          <div className="flex flex-row gap-x-6 overflow-x-auto min-w-full pb-4">
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+          </div>
+        </div>
+        <div className="w-full flex flex-col">
+          <span className="text-palette9 font-Roboto font-semibold text-2xl mb-7">
+            Most Popular
+          </span>
+          <div className="flex flex-row gap-x-6 overflow-x-auto min-w-full pb-4">
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+            <div className="w-[150px] h-[250px] bg-black flex-shrink-0 flex-grow-0"></div>
+          </div>
+        </div>
+      </div>
     </>
-  ) 
+  ); 
 }
  
 export default Home;
