0ec114de28a9bfaf90f1e4966395624a2ad397d6
Maghish <mail4mithran@gmail.com>
2024-04-14 11:45:30 +0530

    Added search bar for Navbar

--- client/src/components/small/Navlinks.tsx
+++ client/src/components/small/Navlinks.tsx
@@ -4,9 +4,7 @@ function Navlinks() {
   return (
     <>
       <div className="ml-[120px] w-full flex items-center gap-5 invisible md:visible">
-        <span className="text-palette2 text-sm font-Roboto cursor-pointer transition-all duration-200 ease-in hover:text-palette5">
-          Explore
-        </span>
+        <input className="text-black text-sm font-Roboto px-4 py-1.5 placeholder:text-palette4 outline-none rounded" placeholder="Search anything..." /> 
         <span className="text-palette2 text-sm font-Roboto cursor-pointer transition-all duration-200 ease-in hover:text-palette5">
           My books
         </span>
